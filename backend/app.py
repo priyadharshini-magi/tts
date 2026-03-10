5e8db0b730f5ecdc3df2534abd5d010952b82ec2
priyadharshini <77532@thescmsilk.com>
2026-03-10 15:33:42 +0530

    added frontend and backend

---
+++ backend/app.py
@@ -0,0 +1,103 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import Optional
+import edge_tts
+import uuid
+import os
+import asyncio
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://172.16.50.24:3000","http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+def speed_to_rate(speed: float) -> str:
+    # 1.0 → +0%
+    # 1.25 → +25%
+    # 0.75 → -25%
+    percent = int((speed - 1.0) * 100)
+    sign = "+" if percent >= 0 else ""
+    return f"{sign}{percent}%"
+
+
+
+OUTPUT_DIR = "generated_audio"
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+
+class TTSRequest(BaseModel):
+    text: str
+    language: str
+    voice: Optional[str] = None
+    speed: Optional[float] = 1.0
+    pitch: Optional[str] = "+0Hz"   
+
+# Language -> Voice mapping
+VOICE_MAP = {
+    "tamil": ["ta-IN-PallaviNeural","ta-IN-ValluvarNeural","ta-LK-SaranyaNeural",]
+,
+    "english": [
+    "en-IN-NeerjaNeural",
+    "en-IN-PrabhatNeural"]
+,
+    "hindi": [
+        "hi-IN-SwaraNeural",
+        "hi-IN-MadhurNeural",]
+,
+    "telugu": ["te-IN-ShrutiNeural","te-IN-MohanNeural","te-IN-ChitraNeural"]
+,
+    "kannada": ["kn-IN-SapnaNeural","kn-IN-VishnuNeural","kn-IN-GowriNeural"]
+,
+    "malayalam": ["ml-IN-SobhanaNeural","ml-IN-MidhunNeural","ml-IN-AnilaNeural"]
+
+}
+
+
+@app.post("/generate")
+async def generate_audio(data: TTSRequest):
+
+    voices = VOICE_MAP.get(data.language)
+    if not voices:
+        return {"error": "Unsupported language"}
+
+    # fallback to first voice
+    selected_voice = data.voice if data.voice in voices else voices[0]
+    rate = speed_to_rate(data.speed or 1.0)
+    pitch = data.pitch or "+0Hz"
+    if not pitch.startswith(("+", "-")):
+        pitch = "+" + pitch
+
+
+    filename = f"{uuid.uuid4()}.wav"
+    filepath = os.path.join(OUTPUT_DIR, filename)
+
+    try:
+        communicate = edge_tts.Communicate(
+            text=data.text,
+            voice=selected_voice,
+            rate=rate,
+            pitch=pitch
+        )
+        await communicate.save(filepath)
+
+    except Exception as e:
+        print("TTS ERROR:", e)
+        return {"error": str(e)}
+
+    return {"file": filename}
+
+
+@app.get("/audio/{filename}")
+def get_audio(filename: str):
+    path = os.path.join(OUTPUT_DIR, filename)
+    return FileResponse(
+    path,
+    media_type="audio/wav",
+    filename=filename,
+    headers={"Content-Disposition": f"attachment; filename={filename}"}
+)
